76d7b333c8eecc6f358c080c93f29705ad0a7e1c
Mehmet Yilmaz <mehmetyilmaz001@gmail.com>
2023-08-04 12:30:09 +0300

    antd implementation

---
+++ src/pages/admin/dashboard/index.jsx
@@ -0,0 +1,15 @@
+import { Button } from 'antd';
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            Admin Dashboard
+            <Button type="primary">Test</Button>
+        </div>
+    );
+};
+
+
+export default Dashboard;
+
