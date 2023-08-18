bfc24769adce6b7e77aa391c05b307cee561d51c
Mehmet Yilmaz <mehmetyilmaz001@gmail.com>
2023-08-18 22:22:30 +0300

    feture: menu and logo changes

--- src/components/layouts/AdminLayout/AdminLayout.tsx
+++ src/components/layouts/AdminLayout/AdminLayout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import { Layout, Menu, Space, theme } from 'antd';
 import { useTranslation } from 'next-i18next';
 import type { MenuProps } from 'antd';
@@ -13,23 +14,23 @@ import {
   VideoCameraOutlined,
 } from '@ant-design/icons';
 import Breadcrump from '@/components/Breadcrump/Breadcrump';
+import { useRouter } from 'next/router';
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const items: MenuProps['items'] = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  AppstoreOutlined,
-  TeamOutlined,
-  ShopOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+
+const menuItems: MenuProps["items"] = [
+  {
+    key: "/admin",
+    icon: <AppstoreOutlined />,
+    label: "Dashboard"
+  },
+  {
+    key: "/admin/users",
+    icon: <UserOutlined />,
+    label: "Users"
+  }
+]
 
 
 interface ILayoutProps {
@@ -40,19 +41,22 @@ interface ILayoutProps {
 const AdminLayout: React.FC<ILayoutProps> = ({ children, showBreadcrump }) => {
   const { t } = useTranslation("common");
 
-  const { token: { colorBgContainer }} = theme.useToken();
-  
+  const { token: { colorBgContainer } } = theme.useToken();
+  const rouuter = useRouter();
+
   return (
     <Layout hasSider className="admin-layout">
-      <Sider className="sider">
-        <div className="logo">Charger Bee Admin</div>
-        <Menu className="menu" theme="light" mode="inline" defaultSelectedKeys={['4']} items={items} />
+      <Sider className="sider" theme='light'>
+        <div className="logo"><Image width={0} height={0} src="/images/logo.svg" alt="logo" sizes='100vh' style={{ width: '100%', height: 'auto' }} /></div>
+        <Menu className="menu" theme="light" mode="inline" defaultSelectedKeys={['4']} items={menuItems} onClick={(menuItem) => {
+          rouuter.push(menuItem.key)
+        }} />
       </Sider>
       <Layout className="">
         <Header style={{ padding: 0, background: colorBgContainer }} />
         <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
           <div style={{ padding: 24, background: colorBgContainer }}>
-            <Space direction="vertical" style={{width: "100%"}}>
+            <Space direction="vertical" style={{ width: "100%" }}>
               {showBreadcrump && <Breadcrump />}
               {children}
             </Space>
